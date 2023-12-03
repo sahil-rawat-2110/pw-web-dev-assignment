de8da5d99858bc43a9eabbccee38946ed2cc2636
Sahil Rawat <124564195+sahil-rawat-2110@users.noreply.github.com>
2023-12-04 01:12:47 +0530

    Add files via upload

---
+++ 27 june crud pw assignment/config/dbConnection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+require("dotenv").config()
+
+const connectToDatabase=async()=>{mongoose.connect(process.env.DB_URL).then((con)=>{
+    console.log("Database Connected Successfully")
+}).catch((err)=>{
+    console.log("Error While Connecting to DB",err.message)
+})}
+
+module.exports ={connectToDatabase};
---
+++ 27 june crud pw assignment/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const { userRoute } = require("./routes/User.route");
+const { connectToDatabase } = require("./config/dbConnection");
+require("dotenv").config()
+
+const app=express();
+
+app.use(cors());
+app.use(express.json()) //middleware to work with json data
+
+app.use("/",userRoute)  // user route
+
+app.listen(process.env.PORT,async()=>{
+    await connectToDatabase()
+    console.log("Server Started on PORT NO:",process.env.PORT)
+})
